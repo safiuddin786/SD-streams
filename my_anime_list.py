1e742877a6d5f1daf7c7a30aaec59140b94f8763
safi <safiuddin786@icloud.com>
2021-04-17 19:52:40 -0400

    new my_anime_list

--- my_anime_list.py
+++ my_anime_list.py
@@ -1,12 +1,14 @@
 from mal import *
 
 def details(anime_name):
+	output = []
 	search = AnimeSearch(anime_name)
 	selected = search.results[0]
 	title = selected.title
-	total_episodes = selected.episodes
-	score = selected.score
+	total_episodes = str(selected.episodes)
+	score = str(selected.score)
 	anime_type = selected.type
 	detail = {'title':title, 'total_episodes':total_episodes, 'score':score, 'anime_type':anime_type}
-	return(detail)
+	output.append(detail)
+	return(output)
 
